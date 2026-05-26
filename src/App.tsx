8495c4af7b96ef9bc511be25fb8061b6a3fee32e
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-05-26 09:57:48 +0530

    seperate name, intro and github components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
 function App() {
 	return (
 		<section>
-			<p>Himanshu Sorathiya</p>
-			<p>
-				Hello world, I'm Himanshu Sorathiya, working at Simform Solutions as
-				Trainee in React department. I completed my BE in IT at Vishvakarma
-				Goverment Engineering College.
-			</p>
-			<p>
-				This is my{" "}
-				<a
-					href="https://github.com/Himanshu-Sorathiya-Simform"
-					target="_blank"
-				>
-					GitHub
-				</a>{" "}
-				account.
-			</p>
+			<Name />
+
+			<Intro />
+
+			<GitHub />
 		</section>
+	);
+}
+
+function Name() {
+	return <p>Himanshu Sorathiya</p>;
+}
+
+function Intro() {
+	return (
+		<p>
+			Hello world, I'm Himanshu Sorathiya, working at Simform Solutions as
+			Trainee in React department. I completed my BE in IT at Vishvakarma
+			Goverment Engineering College.
+		</p>
+	);
+}
+
+function GitHub() {
+	return (
+		<p>
+			This is my{" "}
+			<a
+				href="https://github.com/Himanshu-Sorathiya-Simform"
+				target="_blank"
+			>
+				GitHub
+			</a>{" "}
+			account.
+		</p>
 	);
 }
 
