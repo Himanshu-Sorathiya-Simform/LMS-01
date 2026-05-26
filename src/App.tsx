1f0cc821db2a3bfed2d180fa3b857f0206dc64d2
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-05-26 09:55:49 +0530

    add basic details

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
 function App() {
-	return null;
+	return (
+		<section>
+			<p>Himanshu Sorathiya</p>
+			<p>
+				Hello world, I'm Himanshu Sorathiya, working at Simform Solutions as
+				Trainee in React department. I completed my BE in IT at Vishvakarma
+				Goverment Engineering College.
+			</p>
+			<p>
+				This is my{" "}
+				<a
+					href="https://github.com/Himanshu-Sorathiya-Simform"
+					target="_blank"
+				>
+					GitHub
+				</a>{" "}
+				account.
+			</p>
+		</section>
+	);
 }
 
 export default App;
